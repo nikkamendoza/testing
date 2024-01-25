4c0c135026dffbfbbc581262785da15316c11da9
nikkamendoza <123077713+nikkamendoza@users.noreply.github.com>
2024-01-25 20:23:27 +0800

    using scanner

--- print.java
+++ print.java
@@ -1,7 +1,13 @@
 // Your First Program
+import java.util.Scanner;
 
 class print {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = Scan.nextLine();
+        System.out.println("Hi, " + name + "!");
     }
 }
