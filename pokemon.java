77e8c688cd45ac87a24f46be3c1a9fb4468da330
clareenz <148300455+clareenz@users.noreply.github.com>
2024-02-01 10:21:03 +0800

    Update pokemon.java

--- pokemon.java
+++ pokemon.java
@@ -1,6 +1,18 @@
 public class pokemon {
     public static void main(String[] args) {
         System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        System.out.println("pokemon");
+        
 
         
     }
